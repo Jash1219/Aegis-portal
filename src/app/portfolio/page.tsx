7b2301683e0e2a402fda155ca490c1e601bedeb4
Jash1219 <ashokjashan@gmail.com>
2026-07-01 15:36:56 +0530

    feat(phase7): integrate portfolio screen into app router

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,11 +1,5 @@
-import PortfolioContent from "@/components/portfolio/PortfolioContent";
+import { PortfolioScreen } from "@/components/screens/portfolio";
 
 export default function PortfolioPage() {
-  return (
-    <div className="min-h-screen bg-[#0A0A0A] px-6 pb-8 pt-24">
-      <div className="max-w-7xl mx-auto flex flex-col gap-lg">
-        <PortfolioContent />
-      </div>
-    </div>
-  );
+  return <PortfolioScreen />;
 }
